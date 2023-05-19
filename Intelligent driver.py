3e7aa5d142e0efd0e42b41316ad07437fb1aa920
Kataevskiy <kataevskiymikhail@gmail.com>
2023-05-19 17:32:11 +0200

    v 0.0.7

--- Intelligent driver.py
+++ Intelligent driver.py
@@ -44,22 +44,30 @@ class vecteur:
         return vecteur(self.x**a, self.y**a)
 
 
+ROUGE = 1
+BLEU = 2
+
 class voiture:
 
     voitures = []
-    vitesse_finale = 100
+    vitesse_finale = 200
     distance_minimale = 50
     duree_minimale = 1
     acceletarion_maximale = 100
     deceleration_maximale = 100
 
-    def __init__(self, texture, x, y, angle):
+    def __init__(self, couleur, x, y):
         self.position = vecteur(x, y) # position
-        self.angle = angle # orientation par rapport à la horizontale
         self.vitesse = vecteur(0, 0) # vitesse
         self.acceleration = vecteur(0, 0) # accélération
-        self.texture = texture
-        self.taille = texture.get_height() 
+        self.couleur = couleur
+        if couleur == ROUGE:
+            self.texture = texture_rouge
+            self.angle = 0
+        else:
+            self.texture = texture_bleu
+            self.angle = math.radians(90)
+        self.taille = self.texture.get_height() 
 
     def afficher(self):
         ecran.blit(self.texture, (self.position.x - self.taille // 2, self.position.y - self.taille // 2))
@@ -68,23 +76,24 @@ class voiture:
         min_dist = 10**12
         dv = vecteur(0, 0)
         for autre in voitures:
-            for correction_x in [-1600, 0, 1600]:
-                for correction_y in [-800, 0, 800]:
-                    autre_possibilite = vecteur(correction_x, correction_y) + autre.position
-                    vecteur_distance = autre_possibilite - self.position
-                    vecteur_direction = vecteur(math.cos(self.angle), math.sin(self.angle))
-                    projete = vecteur.produit_scalaire(vecteur_distance, vecteur_direction)
-                    distance_au_projete = vecteur.distance_euclidienne(vecteur_distance, vecteur_direction * projete)
-                    if projete > self.taille and distance_au_projete < (1 * self.taille) and projete < min_dist:
-                        min_dist = projete
-                        dv = self.vitesse - autre.vitesse
+            d_pos = autre.position - self.position
+            if self.couleur == ROUGE:
+                d_pos.x %= 1600
+                if d_pos.x > self.taille and abs(d_pos.y) < self.taille * 1:
+                    min_dist = min(d_pos.x, min_dist)
+                    dv = self.vitesse - autre.vitesse
+            else:
+                d_pos.y %= 800
+                if d_pos.y > self.taille and abs(d_pos.x) < self.taille * 1:
+                    min_dist = min(d_pos.y, min_dist)
+                    dv = self.vitesse - autre.vitesse
         return min_dist, dv
     
     def nouvelle_acceleration(self, dist, dv, v_max, d_min, t_min, a_max, da_max):
-        if self.vitesse.x < -self.vitesse_finale / 10:
-            self.vitesse.x = -self.vitesse_finale / 10
-        if self.vitesse.y < -self.vitesse_finale / 10:
-            self.vitesse.y = -self.vitesse_finale / 10
+        if self.vitesse.x < 0:
+            self.vitesse.x = 0
+        if self.vitesse.y < 0:
+            self.vitesse.y = 0
         facteur_libre = (vecteur(1, 1) - (self.vitesse / v_max)**4) * a_max * vecteur(math.cos(self.angle), math.sin(self.angle))
         facteur_interaction = ((vecteur(d_min, d_min) + self.vitesse * t_min + (self.vitesse * dv / (2 * math.sqrt(a_max * da_max)))) / dist)**2 * (-a_max) * vecteur(math.cos(self.angle), math.sin(self.angle))
         return facteur_libre + facteur_interaction
@@ -98,17 +107,25 @@ class voiture:
 
         self.position.x %= 1600 # si on dépasse l'écran, on reapparait de l'autre côté
         self.position.y %= 800
-        
+
 for i in range(10):
-    voiture.voitures.append(voiture(texture_rouge, 100 + 50 * i, 390 - 19, 0))
-    voiture.voitures.append(voiture(texture_rouge, 100 + 50 * i, 390 + 19 + 19, 0))
-for i in range(5):
-    voiture.voitures.append(voiture(texture_bleu, 390 - 19, 100 + 50 * i, math.radians(90)))
-    voiture.voitures.append(voiture(texture_bleu, 390 + 19 + 19, 100 + 50 * i, math.radians(90)))
-    voiture.voitures.append(voiture(texture_bleu, 790 - 19, 100 + 50 * i, math.radians(90)))
-    voiture.voitures.append(voiture(texture_bleu, 790 + 19 + 19, 100 + 50 * i, math.radians(90)))
-    voiture.voitures.append(voiture(texture_bleu, 1190 - 19, 100 + 50 * i, math.radians(90)))
-    voiture.voitures.append(voiture(texture_bleu, 1190 + 19 + 19, 100 + 50 * i, math.radians(90)))
+    voiture.voitures.append(voiture(ROUGE, 100 + 50 * i, 390 - 19))
+    voiture.voitures.append(voiture(ROUGE, 100 + 50 * i, 390))
+    voiture.voitures.append(voiture(ROUGE, 100 + 50 * i, 390 + 19))
+    voiture.voitures.append(voiture(ROUGE, 100 + 50 * i, 390 + 19 + 19))
+for i in range(10):
+    voiture.voitures.append(voiture(BLEU, 390 - 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 390, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 390 + 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 390 + 19 + 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 790 - 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 790, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 790 + 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 790 + 19 + 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 1190 - 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 1190, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 1190 + 19, 100 + 50 * i))
+    voiture.voitures.append(voiture(BLEU, 1190 + 19 + 19, 100 + 50 * i))
 
 while active:
     dt = horloge.tick(165)/1000  # limite FPS to 165
